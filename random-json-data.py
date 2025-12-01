dedf3c7eaedcf1fb28e07c205924e9d7b01971dd
Vladimir Shkurletov <89521087703@mail.ru>
2025-12-01 12:30:14 +0000

    wrap the code in a main function and add comments for others functions

--- random-json-data.py
+++ random-json-data.py
@@ -4,21 +4,24 @@ import sys
 import datetime as dt
 import argparse 
 
-#Инициализация
+# Запись в файл
 def writeToFile(filePath, text):
     print(f"Производится запись сгенерированного json в файл {filePath}")
     with open(filePath, 'w') as file:
         file.write(text)
 
+# Генерация случайного номера мобильного телефона
 def genMobPhoneRand():
     phoneNum='+7'
     for i in range(10): phoneNum+=str(rnd.randrange(10))
     return phoneNum
 
+# Генерация даты рождения 
 def genBirthyear():
     nowYear = dt.datetime.now().year
     return rnd.randrange(nowYear-100, nowYear+1)
  
+# Простая транслитерация РУС->АНГЛ
 def simpleTransliterateRusToEn(word):
     word = word.lower()
     dictLiters = {
@@ -63,6 +66,7 @@ def simpleTransliterateRusToEn(word):
     
     return translit
 
+# Генерация случайного адреса электронной почты
 def genEmail(lastName, firstName, birthyear):
     methodInd = rnd.randrange(3)
     domain = rnd.choice(("yandex.ru", "mail.ru", "gmail.com"))
@@ -78,58 +82,65 @@ def genEmail(lastName, firstName, birthyear):
     return email
 
 
-jsonRes=[]
+def main():
+     
+    jsonRes=[]
+    lastNameM = ["Васечкин", "Петров", "Сидоров", "Кожемяков", "Аскользкий", "Баринов", "Волынов", "Гусев", "Деревянко", "Ежов", "Изваров", "Коломыйцев", "Лебедев", "Марков", "Носов", "Котов"]
+    firstNameM = ["Василий", "Петр", "Владимир", "Константин", "Борис", "Антон", "Валерий", "Михаил", "Олег", "Александр", "Тимофей", "Максим", "Павел", "Юрий", "Даниил", "Сергей", "Андрей", "Алексей"]
+    lastNameF = ["Анютова", "Котейкина", "Сокольникова", "Мясоедова", "Лопатина", "Зарубина", "Смородинова", "Павлова", "Кузнецова", "Сидорова", "Мельникова", "Виноградова", "Морозова", "Ковалёва"]
+    firstNameF = ["Елена", "Ирина", "Мария", "Юлия", "Анастасия", "Виктория", "Екатерина", "Дарья", "Ирина", "Елизавета", "Лариса", "Вера",  "Анна", "Любовь", "Людмила", "Надежда", "Наталья", "Ольга", "Нина"]
+    city = ["Воронеж", "Москва", "Санкт-Петербург", "Волгоград", "Кисловодск", "Белгород", "Калуга", "Новосибирск", "Самара", "Казань", "Челябинск", "Ростов-на-Дону", "Уфа", "Екатеринбург", "Омск", "Нижний Новгород"]
+    old = range(101)
+    gender = ["Мужчина", "Женщина"]
 
-lastNameM = ["Васечкин", "Петров", "Сидоров", "Кожемяков", "Аскользкий", "Баринов", "Волынов", "Гусев", "Деревянко", "Ежов", "Изваров", "Коломыйцев", "Лебедев", "Марков", "Носов", "Котов"]
-firstNameM = ["Василий", "Петр", "Владимир", "Константин", "Борис", "Антон", "Валерий", "Михаил", "Олег", "Александр", "Тимофей", "Максим", "Павел", "Юрий", "Даниил", "Сергей", "Андрей", "Алексей"]
-lastNameF = ["Анютова", "Котейкина", "Сокольникова", "Мясоедова", "Лопатина", "Зарубина", "Смородинова", "Павлова", "Кузнецова", "Сидорова", "Мельникова", "Виноградова", "Морозова", "Ковалёва"]
-firstNameF = ["Елена", "Ирина", "Мария", "Юлия", "Анастасия", "Виктория", "Екатерина", "Дарья", "Ирина", "Елизавета", "Лариса", "Вера",  "Анна", "Любовь", "Людмила", "Надежда", "Наталья", "Ольга", "Нина"]
-city = ["Воронеж", "Москва", "Санкт-Петербург", "Волгоград", "Кисловодск", "Белгород", "Калуга", "Новосибирск", "Самара", "Казань", "Челябинск", "Ростов-на-Дону", "Уфа", "Екатеринбург", "Омск", "Нижний Новгород"]
-old = range(101)
-gender = ["Мужчина", "Женщина"]
+   #Работа с аргументами
+    parser = argparse.ArgumentParser(
+                description='',
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                epilog="""
+                       Примеры использования:
+                        """
+            )        
+    parser.add_argument('--count', required=False, type=int, default=1, help='Количество объектов в выходном JSON')
+    parser.add_argument('--file', required=False, type=str, help='Путь к файлу, куда требуется записать json')
+    args = parser.parse_args()
+    resCount=args.count
+    filePath=args.file
 
-#Работа с аргументами
-parser = argparse.ArgumentParser(
-            description='',
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            epilog="""
-                   Примеры использования:
-                    """
-        )        
-parser.add_argument('--count', required=False, type=int, default=1, help='Количество объектов в выходном JSON')
-parser.add_argument('--file', required=False, type=str, help='Путь к файлу, куда требуется записать json')
-args = parser.parse_args()
-resCount=args.count
-filePath=args.file
+    print(f"ARGS:\n\tcount:{resCount}\n\tfilePath:{filePath}")
 
-print(f"ARGS:\n\tcount:{resCount}\n\tfilePath:{filePath}")
+    #Генерация
+    for i in range(resCount):
+        gendRand = rnd.choice(gender)
+        if gendRand == gender[0]:
+            LNRand = rnd.choice(lastNameM)
+            FNRand = rnd.choice(firstNameM)
+        else:
+            LNRand = rnd.choice(lastNameF)
+            FNRand = rnd.choice(firstNameF)
+        cityRand = rnd.choice(city)
+        mobPhoneRand = genMobPhoneRand()
+        birtyear = genBirthyear()
+        email = genEmail(LNRand, FNRand, birtyear)
+        jsonRes.append({
+                "LastName": LNRand,
+                "FirstName": FNRand,
+                "Gender": gendRand,
+                "City": cityRand,
+                "Mobile Phone Number": mobPhoneRand,
+                "Email": email,
+                "Birth year": birtyear
+                })
 
-#Генерация
-for i in range(resCount):
-    gendRand = rnd.choice(gender)
-    if gendRand == gender[0]:
-        LNRand = rnd.choice(lastNameM)
-        FNRand = rnd.choice(firstNameM)
+    #Вывод результата
+    print("JSON:\n")
+    if filePath:
+        writeToFile(filePath, json.dumps(jsonRes, ensure_ascii=False))
     else:
-        LNRand = rnd.choice(lastNameF)
-        FNRand = rnd.choice(firstNameF)
-    cityRand = rnd.choice(city)
-    mobPhoneRand = genMobPhoneRand()
-    birtyear = genBirthyear()
-    email = genEmail(LNRand, FNRand, birtyear)
-    jsonRes.append({
-            "LastName": LNRand,
-            "FirstName": FNRand,
-            "Gender": gendRand,
-            "City": cityRand,
-            "Mobile Phone Number": mobPhoneRand,
-            "Email": email,
-            "Birth year": birtyear
-            })
+        print(json.dumps(jsonRes, ensure_ascii=False))
 
-#Вывод результата
-print("JSON:\n")
-if filePath:
-    writeToFile(filePath, json.dumps(jsonRes, ensure_ascii=False))
-else:
-    print(json.dumps(jsonRes, ensure_ascii=False))
+if __name__=="__main__":
+    try:
+        main()
+    except Exception as E:
+        print(f"Неизвестная ошибка: {e}")
